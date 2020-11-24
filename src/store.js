8d44f9756cbda4fcb75517ab4bba3e77f63b9548
archeo3d <bricea75@hotmail.com>
2020-11-24 12:44:04 +0100

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,21 @@
+import create from "zustand";
+import { Vector3 } from "three";
+
+const useStore = create((set, get) => ({
+  orbitEnabled: true,
+  position: new Vector3(),
+  direction: new Vector3(),
+  // mutation : {
+  //   position : new Vector3(),
+  //   direction : new Vector3(),
+  // },
+  actions: {
+    toggleOrbit(orbitEnabled = !get().orbitEnabled) {
+      set({ orbitEnabled });
+    }
+  },
+  setPosition: (position) => set({ position }),
+  setDirection: (direction) => set({ direction })
+}));
+
+export { useStore };
